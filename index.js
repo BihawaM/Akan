48d2e36fab1c57058ecd2e1db3ceac11bc737ed2
BihawaM <bihawam271@gmail.com>
2020-08-23 17:43:55 +0300

    create variables

---
+++ index.js
@@ -0,0 +1,5 @@
+var cc, year, month, dayOfMonth, dayOfBirth, day, gender, genderval;
+var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+var DD = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
+
