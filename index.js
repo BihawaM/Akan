901072610661201e8158db9d3fa8f1b65639bd7c
BihawaM <bihawam271@gmail.com>
2020-08-23 17:45:42 +0300

    create input function

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
 var DD = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
 
+function myInput() {
+    cc = parseInt(document.getElementById("cc").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("day-of-month").value);
+}
+
